871d6b85a312a090eeb3967102965a376e9f7a06
Mr.Steven <vanhungbkcbg1@gmail.com>
2017-02-03 11:27:31 +0700

    event laravel

---
+++ app/Listeners/AddMyListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\MyEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AddMyListener implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  MyEvent  $event
+     * @return void
+     */
+    public function handle(MyEvent $event)
+    {
+        //
+    }
+}
