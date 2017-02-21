18dc1c46e91857bf63244c1c9471d8d69261fec4
Mr.Steven <vanhungbkcbg1@gmail.com>
2017-02-21 21:54:01 +0700

    multi ajax call and call back when all ajax request done

--- resources/views/ajax_multi_request/index.blade.php
+++ resources/views/ajax_multi_request/index.blade.php
@@ -5,14 +5,24 @@
     <script type="text/javascript">
         $("#send").click(function () {
 
-            var a= $.get('/request1');
-            var b= $.get('/request2');
+            var a= $.get('/request1ss').done(function(data){
+
+                debugger;
+                console.log(data);
+            });
+            var b= $.get('/request2').done(function (data) {
+                debugger;
+                console.log(data);
+            });
 
             $.when(a,b).done(function (a,b) {
                 debugger;
                 console.log(a);
                 console.log(b);
 
+            }).fail(function (e) {
+                debugger;
+                console.log(e);
             })
         });
 
